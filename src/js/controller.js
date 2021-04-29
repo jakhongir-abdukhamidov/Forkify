6f693d1a2f35671105cfbe699d968f48581b3741
jakhongir001 <jakhongirabdukhamidov@gmail.com>
2021-04-29 17:16:45 +0500

    implement error handling and success  messages

--- src/js/controller.js
+++ src/js/controller.js
@@ -20,7 +20,7 @@ const controlRecipes = async function () {
     // Render recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    console.error(err);
+    recipeView.renderError();
   }
 };
 
--- src/js/model.js
+++ src/js/model.js
@@ -23,6 +23,6 @@ export const loadRecipe = async function (id) {
     console.log(data);
     console.log(state.recipe);
   } catch (err) {
-    console.error(err);
+    throw err;
   }
 };
